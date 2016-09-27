4e5a0356517eeb1ab559c8d69c36d778bca674ee
gmlbarus <gmlbarus@icloud.com>
2016-09-28 01:38:14 +0700

    Create data order page for user, admin , and manager

--- admin/modul/mod_laporan-pemesanan/order.php
+++ admin/modul/mod_laporan-pemesanan/order.php
@@ -6,250 +6,318 @@ if (empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
 	echo "<a href=../../index.php><b>LOGIN</b></a></center>";
 }
 else{
-	
-	switch($_GET[act]){
-	// Tampil Order
-	default:
-		echo "<h2>Laporan Pemesanan</h2>
-			  <form method='POST' action='".$_SERVER[PHP_SELF].'?module=laporan-pemesanan'."'>
-			  <table>
-			  <tr>
-				<td width=120>
-					Kelas : 
-					<select name='kelas'>";
-					$tampil=mysql_query("SELECT deskripsi FROM konten WHERE grup='kelas' ORDER BY deskripsi");
-					echo "<option value='Semua'>Semua</option>";
-					while($r=mysql_fetch_array($tampil)){
-					  if($_POST[kelas]==$r[deskripsi]) {
-						echo "<option value='$r[deskripsi]' selected>$r[deskripsi]</option>";
-					  }
-					  else{
-						echo "<option value=$r[deskripsi]>$r[deskripsi]</option>";
-					  }
-					}
-					echo "</select>
-				</td>
-				<td width=270>
-					Periode :
-					<input name='dari' size='10' type='text' id='dari' value='$_POST[dari]'/>
-					<script>
-						Calendar.setup({
-							trigger    : 'dari',
-							inputField : 'dari'
-						});
-					</script>
-					<input name='sampai' size='10' type='text' id='sampai' value='$_POST[sampai]'/>
-					<script>
-						Calendar.setup({
-							trigger    : 'sampai',
-							inputField : 'sampai'
-						});
-					</script>
-				<td>
-				<td>
-					<input type='submit' value='Lihat Data' name='lihat'>
-				</td>
-			  </tr>
-			  </table>
-			  </form>";
-		
-		if($_POST[lihat]!=''){
-			echo "
-				  <table>
-				  <tr><th>#</th><th>No. Invoice</th><th>Nama Pelanggan</th><th>Trip</th><th>Kelas</th><th>Tanggal Order</th><th align='center'>Jumlah Tiket</th><th>Total Bayar</th><th>Status</th><th>Aksi</th></tr>";
+	?>
 
-			if($_POST[kelas]!='Semua'){
-				$teks = "and k.kelas='".$_POST[kelas]."'";
-			}
-			
-			$query = mysql_query("select *,
-								(select sum(qty) from order_detail where id_order=o.id_order) as jml,
-								k.kelas,
-								coalesce((select sum((tarif*qty)-(tarif*qty*diskon)) from order_detail where id_order=o.id_order),0) +
-								coalesce((select sum(tarif) from order_meal where id_order=o.id_order),0) +
-								coalesce((select sum(tarif) from order_pijat where id_order=o.id_order),0) as total
-								from `order` o 
-								inner join order_kelas k on o.id_order=k.id_order and k.aktif='Y' $teks
-								where date(o.tgl_input) between '".$_POST[dari]."' and '".$_POST[sampai]."'
-								order by o.tgl_input desc");
+	<script language="javascript">
+	$(document).ready(function() {
+		$('#tabel-tiket').dataTable( {
+			"order": [[ 0, "asc" ]]
+		} );
+	} );</script>
 
-			$no = 1;
-			while($q = mysql_fetch_array($query)){
-				$kelas = '';
-				$query_u = mysql_query("select * from order_kelas where id_order='$q[id_order]'");
-				while($u = mysql_fetch_array($query_u)){
-					$kelas .= '^'.$u[kelas];
-				}
-				$kelas = right($kelas, strlen($kelas)-1);
-				$kelas = str_replace("^","<img src='images/up.png'>",$kelas);
-				
-				if($q[trip]=='pp') $trip = 'Pulang Pergi';
-				if($q[trip]=='one_way') $trip = 'Sekali Jalan';
-				
-				$jml = $q[jml];
-				if($trip=='Pulang Pergi') $jml = ($q[jml])/2;
-				$total = $q[total];
-				echo "
+	<h1>Data Order</h1>
+	<hr color='#c5a430' size='1'>
+
+	<?php
+	$query 	= mysql_query("SELECT `a`.*, `b`.`nama` FROM `order` `a` JOIN `profil` `b`
+		ON `a`.`username`=`b`.`email`");
+	$jml	= mysql_num_rows($query);
+	if($jml>0){
+		echo '<table id="tabel-tiket" class="display" cellspacing="0" width="100%">
+				<thead>
 					<tr>
-						<td>$no</td>
-						<td>$q[invoice]</td>
-						<td>$q[nama]</td>
-						<td>$trip</td>
-						<td>$kelas</td>
-						<td>$q[tgl_input]</td>
-						<td align='center'>$jml</td>
-						<td>Rp. ".format_rupiah($total)."</td>
-						<td>$q[status]</td>
-						<td><a href='?module=laporan-pemesanan&act=detail&id=$q[id_order]'><b>Baca</b></a></td>
-					</tr>";
-				$no++;
-				$tiket = $tiket+$jml;
-				$uang = $uang+$total;
+						<th>No.</th>
+						<th>Nama</th>
+						<th>No. Invoice</th>
+						<th>Tanggal Jatuh Tempo</th>
+						<th>Status</th>
+						<th>Detail</th>
+					</tr>
+				</thead>
+				<tbody>';
+		$no = 1;
+		while($r=mysql_fetch_array($query)){
+			$img	= "<a href='media.php?module=pemesanan&page=tiket&mode=detail&id=$r[invoice]' title='Lihat detail'><img src='../images/detail.png'></a>";
+			if($r[status]=='Belum Lunas') {
+				$status = 'Pending';
 			}
-			echo "
-			<tr>
-				<td colspan='6' align='right'>Total</td>
-				<td align='center'>$tiket</td>
-				<td><b>Rp. ".format_rupiah($uang)."</b></td>
-				<td></td>
-				<td></td>
-			</tr>";
-			
-			echo "</table>";
+			else if($r[status]=='Lunas') {
+				$status = 'Ready';
+				$img 	.= "<a href=\"javascript:win = window.open('print.php?id=".$r['invoice']."');win.print()\" title=\"Cetak Tiket\"> <img src='images/print.png'></a>";
+			}
+
+			echo "<tr>
+					<td width=20>$no</td>
+					<td>".strtoupper($r[nama])."</td>
+					<td>".strtoupper($r[invoice])."</td>";
+					echo "
+					<td align='center'>".$r[jatuh_tempo]."</td>
+					<td>".strtoupper($r[konfirmasi])."</td>
+					<td align='center'>$img</td>
+				</tr>";
+
+			$no++;
 		}
-	break;
+		echo '	</tbody></table>';
+	}
+	else{
+		echo "<p>Tidak ada data yang ditampilkan.</p>";
+	}
 
 
-	case "detail":		
-		$edit 		= mysql_query("SELECT * FROM `order` WHERE id_order='$_GET[id]'");
-		$r    		= mysql_fetch_array($edit);
-
-		if($r[trip]=='pp') $trip = 'Pulang Pergi';
-		if($r[trip]=='one_way') $trip = 'Sekali Jalan';
-		
-		$query_k	= mysql_query("select * from order_seat where id_order='$_GET[id]' group by seat");
-		$jml_k = 0;
-		while($k = mysql_fetch_array($query_k)){
-			$kursi .= $k[seat].' | ';
-			$jml_k++;
+	if($_GET[mode]=='detail'){
+		function index($teks){
+			switch($teks){
+				case 'EKONOMI':
+					$string = 1;
+					break;
+				case 'EKSEKUTIF':
+					$string = 2;
+					break;
+				case 'VIP':
+					$string = 3;
+					break;
+			}
+			return $string;
 		}
 
-		echo "<h2>Detail Pemesanan</h2>
-			  <table>
-			  <tr><td width=150>No. Invoice</td><td> : $r[invoice]</td></tr>
-			  <tr><td>Tanggal Order</td> 		<td> : $r[tgl_input]</td></tr>
-			  <tr><td>Tipe Trip</td> 			<td> : $trip</td></tr>
-			  <tr><td>Nomor Kursi</td> 			<td> : <b>$kursi</b></td></tr>
-			  <tr><td>Status</td>				<td> : <b>".strtoupper($r[status])."</b></td></tr>
-			  </table>";
-		
-			//Tampilkan detail
-			echo "
-			<table>
+		if($_POST[simpan] != ''){
+			$kelas_temp	= $_POST[kelas_temp];
+			$kelas		= $_POST[kelas];
+
+			if(index($kelas_temp) < index($kelas)){
+				mysql_query("update `order` set pijat='Y' where id_order='$_GET[id]'");
+				mysql_query("update order_kelas set aktif='N' where id_order='$_GET[id]'");
+				mysql_query("insert into order_kelas (id_order,kelas) values ('$_GET[id]','$kelas')");
+
+				$query_u = mysql_query("select * from order_detail where id_order='$_GET[id]'");
+				while($u = mysql_fetch_array($query_u)){
+					mysql_query("update order_detail set tarif=(select $_POST[trip] from tarif where kelas='$kelas' and kategori_penumpang='$u[kategori]')
+								where tgl_berangkat='$u[tgl_berangkat]' and kategori='$u[kategori]' and id_order='$_GET[id]'");
+				}
+
+				mysql_query("update order_pijat set tarif='0' where id_order='$_GET[id]'");
+
+				$cek_p = mysql_query("select * from order_seat where id_order='$_GET[id]'");
+				while($p = mysql_fetch_array($cek_p)){
+					$kat[] = $p[kategori];
+					$nam[] = $p[nama];
+					$jk[]  = $p[jk];
+				}
+				mysql_query("delete from order_seat where id_order='$_GET[id]'");
+
+				$query_tgl = mysql_query("select * from order_detail where id_order='$_GET[id]' group by rute order by tgl_berangkat");
+				for($t=0; $t<mysql_num_rows($query_tgl); $t++){
+					for($u=0; $u<$_POST[jmlkursi]; $u++){
+						mysql_query("insert into order_seat (id_order,seat,kategori,nama,jk,tgl_berangkat)
+						values ('$_GET[id]','".$_POST['seat_'.$u]."','".$kat[$u]."','".$nam[$u]."','".$jk[$u]."','".mysql_result($query_tgl,$t,'tgl_berangkat')."')");
+					}
+				}
+
+				echo "<script>alert('Proses upgrade class anda telah disimpan. Selamat anda mendapatkan promo pijat gratis. Terima kasih !');
+				window.location = 'media.php?page=tiket'</script>";
+			}
+			else{
+				echo "<script>alert('Maaf, proses downgrade class tidak diperbolehkan'); </script>";
+			}
+
+		}
+
+		$query_o 	= mysql_query("SELECT `a`.*, `b`.`nama` FROM `order` `a` JOIN `profil` `b`
+			ON `a`.`username`=`b`.`email`	WHERE `invoice` = '{$_GET[id]}'");
+		$o			= mysql_fetch_array($query_o);
+
+		if($_POST[kelas]==''){
+			$kelas_temp1 = $o[kelas];
+		}
+		else{
+			$kelas_temp1 = $_POST[kelas];
+		}
+
+		if($o[status]=='Belum Lunas'){
+			$tombol = "<input type='submit' value='Upgrade Kelas' name='upgrade'>
+						<br>Dapatkan layanan pijat <b>GRATIS</b> dengan mengupgrade kelas anda !!!<br>
+						atau dengan menambahkan uang Rp50.000";
+
+		}
+		elseif($o[status]=='Lunas'){
+			$tombol = "";
+		}
+
+		echo "
+		<hr color='#c5a430' size='1'>
+		<br><br>
+		<h1>Detail Order</h1>
+		<hr color='#c5a430' size='4'>
+		<form name='form' action='' method='post'>
+		<table cellpadding=0 border=0 width=670>
 			<tbody>
 			<tr>
-				<th>No.</th>
-				<th>Rute & Tanggal Keberangkatan</th>
-				<th>Kategori</th>
-				<th>Jumlah</th>
-				<th>Tarif</th>
-				<th>Diskon</th>
-				<th>Jumlah</th>
-			</tr>";
-			
-			$query_p = mysql_query("select * from order_detail where id_order='$_GET[id]'");
-			while($p = mysql_fetch_array($query_p)){
-				$param[]	= $p[rute];
-				$kategori[]	= $p[kategori];
-				$qty[]		= $p[qty];
-				$tarif[]	= $p[tarif];
-				$diskon[]	= $p[diskon];
-				$total[]	= (($p[qty]*$p[tarif])-($p[qty]*$p[tarif])*$p[diskon]);
-			}
-			
-			$query_d = mysql_query("select rute,tgl_berangkat from order_detail where id_order='$_GET[id]' group by rute order by tgl_berangkat");
-			$jml_d	 = mysql_num_rows($query_d);
-			
-			while($d = mysql_fetch_array($query_d)){
-				$rute[]	= $d[rute];
-				$tgl[]	= $d[tgl_berangkat];
-			}
-			
-			if($trip=='Sekali Jalan') $rowspan = 0;
-			if($trip=='Pulang Pergi') $rowspan = count($kategori)/$jml_d;
-			$rowspan = "rowspan='".$rowspan."'";
-			
-			$urut = 0;
-			for($i=0; $i<count($kategori); $i++){
-				if($diskon[$i]=='0') $teks_diskon = '0';
-				if($diskon[$i]=='0.1') $teks_diskon = '10%';
-				
-				echo "<tr>";
-				if($param[$i] != $param[($i-1)]){
-					echo "<td width='30' $rowspan>".($urut+1)."</td>";
-					echo "<td $rowspan><b>".$rute[$urut]."</b><br><i>".tgl_indo($tgl[$urut])."</i></td>";
-					$urut++;
-				}					
-				echo "
-					<td>".$kategori[$i]."</td>
-					<td>".$qty[$i]." Orang</td>
-					<td>Rp. ".format_rupiah($tarif[$i])."</td>
-					<td>".$teks_diskon."</td>
-					<td>Rp. ".format_rupiah($total[$i])."</td>
-				</tr>
-				";
-				$total_tarif = ($total_tarif + $total[$i]);
-			}
-			
-			echo "
+				<td width=150>Nama Pelanggan</td>
+				<td width=10>:</td>
+				<td><b>".strtoupper($o[nama])."</b></td>
+			</tr>
 			<tr>
-				<td colspan='6' align='right'><b>SUB TOTAL : </b></td>
-				<td><b>Rp. ".format_rupiah($total_tarif)."</b></td>
+				<td width=150>Nomor Invoice</td>
+				<td width=10>:</td>
+				<td><b>$o[invoice]</b></td>
 			</tr>
-			<tr style='border-bottom: 1px solid #c5a430; text-align:left;'>
-				<th>No.</th>
-				<th colspan=5>Layanan Ekstra</th>
-				<th>Jumlah</th>
+			<tr>
+				<td width=150>Koran Utama</td>
+				<td width=10>:</td>
+				<td><b>RIAU POS</b></td>
 			</tr>
-			";
-			$query_m = mysql_query("select o.*,m.nama from order_meal o inner join makanan m on m.id_makanan=o.id_makanan where o.id_order='$_GET[id]'");
-			$no = 1;
-			while($m = mysql_fetch_array($query_m)){
-				echo "
-				<tr>
-					<td>$no</td>
-					<td colspan=5>$m[nama]</td>
-					<td>Rp. ".format_rupiah($m[tarif])."</td>
+			<tr>
+				<td width=150>Koran Tambahan 1</td>
+				<td width=10>:</td>
+				<td><b>".strtoupper($o[koran_1])."</b></td>
+			</tr>
+			<tr>
+				<td width=150>Koran Tambahan 2</td>
+				<td width=10>:</td>
+				<td><b>".strtoupper($o[koran_2])."</b></td>
+			</tr>
+			<tr>
+				<td width=150>Pembayaran Via</td>
+				<td width=10>:</td>
+				<td><b>".strtoupper($o[pembayaran_via])."</b></td>
+			</tr>
+			<tr>
+				<td width=150>Sebanyak</td>
+				<td width=10>:</td>
+				<td><b>$o[sebanyak] x</b></td>
+			</tr>
+			<tr>
+				<td width=150>Masa Aktif</td>
+				<td width=10>:</td>
+				<td><b>$o[masa_aktif] hari</b></td>
+			</tr>
+			<tr>
+				<td width=150>Jatuh Tempo</td>
+				<td width=10>:</td>
+				<td><b>$o[jatuh_tempo]</b></td>
+			</tr>
+			<tr>
+				<td width=150>Status Pembayaran</td>
+				<td width=10>:</td>
+				<td><b>".strtoupper($o[konfirmasi])."</b></td>
+			</tr>
+			<!--tr>
+				<td width=150>Kelas</td>
+				<td width=10>:</td>
+				<td>
+					<input type='hidden' name='kelas_temp' value='$o[kelas]'>
+					<input type='hidden' name='trip' value='$o[trip]'>";
+					if($o[status]=='Belum Lunas'){
+						echo "<select name='kelas'>";
+						$tampil=mysql_query("select * from konten where grup='kelas'");
+						while($t=mysql_fetch_array($tampil)){
+							if(strtoupper($t[deskripsi])==$kelas_temp1){
+								echo "<option value='".strtoupper($t[deskripsi])."' selected>".strtoupper($t[deskripsi])."</option>";
+							}
+							else{
+								echo "<option value='".strtoupper($t[deskripsi])."'>".strtoupper($t[deskripsi])."</option>";
+							}
+						}
+						echo "	</select>";
+					}
+					else{
+						echo "<b>".$kelas_temp1."</b>";
+					}
+
+			 echo "$tombol
+				</td>
+			</tr>
+			<tr>
+				<td width=150>Tipe Trip</td>
+				<td width=10>:</td>
+				<td><b>";
+				if($o[trip]=='pp'){
+					echo 'Pulang Pergi';
+				}
+				else if($o[trip]=='one_way'){
+					echo 'Sekali Jalan';
+				}
+			echo "</b></td>
+			</tr-->
+			<tr>
+				<td width=150>Total Bayar</td>
+				<td width=10>:</td>
+				<td><b>";
+		$pe = mysql_fetch_array(mysql_query("select coalesce(sum(((qty*tarif) - ((qty*tarif)*diskon))),0) as total from order_detail where id_order=$o[id_order]"));
+		$m 	= mysql_fetch_array(mysql_query("select coalesce(sum(tarif),0) as total from order_meal where id_order=$o[id_order]"));
+		$pi = mysql_fetch_array(mysql_query("select coalesce(sum(tarif),0) as total from order_pijat where id_order=$o[id_order]"));
+
+		echo 'Rp. '.format_rupiah($o[harga]);
+
+		echo "</b></td>
+			</tr>
+			</tbody>
+		</table>";
+
+		if($_POST[upgrade] != ''){
+			$jmlpenumpang = mysql_query("select rute,sum(qty) as qty from order_detail where id_order='$_GET[id]' group by rute");
+			if(index($_POST[kelas_temp]) < index($_POST[kelas])){
+				echo "<table cellpadding='0' border='1' width='670'>";
+
+				if($_POST[kelas]=='EKSEKUTIF'){
+					for($i=0; $i<=2; $i++){
+						if($i==0) $var = 'A';
+						if($i==1) $var = 'B';
+						if($i==2) $var = 'C';
+						$cektanggal = mysql_query("select * from order_detail where id_order='$_GET[id]'");
+						$query_seat = mysql_query("select os.seat,os.tgl_berangkat,ok.kelas from order_seat os
+													 inner join order_kelas ok on os.id_order=ok.id_order and ok.aktif='Y' and ok.kelas='$_POST[kelas]'
+													 where os.tgl_berangkat='".mysql_result($cektanggal,0,'tgl_berangkat')."'");
+
+						unset($seat_disable);
+						while($s = mysql_fetch_array($query_seat)){
+							$seat_disable[] = $s[seat];
+						}
+
+						echo "
+						<tr>
+							<td style='padding: 10px 0px 10px 0px; text-align:center'><b>EKSEKUTIF $var</b>";
+							seat('eksekutif '.$var, $seat_disable);
+						echo "</td>
+						</tr>";
+					}
+				}
+				else{
+					echo "<tr>
+						<td style='padding: 10px 0px 10px 0px; text-align:center'> <b>$_POST[kelas]</b>";
+						$cektanggal = mysql_query("select * from order_detail where id_order='$_GET[id]'");
+						$query_seat = mysql_query("select os.seat,os.tgl_berangkat,ok.kelas from order_seat os
+													 inner join order_kelas ok on os.id_order=ok.id_order and ok.aktif='Y' and ok.kelas='$_POST[kelas]'
+													 where os.tgl_berangkat='".mysql_result($cektanggal,0,'tgl_berangkat')."'");
+						unset($seat_disable);
+						while($s = mysql_fetch_array($query_seat)){
+							$seat_disable[] = $s[seat];
+						}
+
+						seat($_POST[kelas], $seat_disable);
+					echo "</td>
+					</tr>";
+				}
+				echo "<tr>
+					<td style='padding: 10px 0px 10px 0px; text-align:center'>
+						<input name='jmlkursi' id='jmlkursi' type='hidden' value='".mysql_result($jmlpenumpang,0,'qty')."' />
+						<input type='submit' value='Simpan' name='simpan' onclick=\"return validasi()\">
+					</td>
 				</tr>";
-				$total_ekstra = ($total_ekstra + $m[tarif]);
-				$no++;
+				echo "</table>";
+				for($i=0;$i<mysql_result($jmlpenumpang,0,'qty');$i++){
+					echo "<input name='seat_$i' id='seat_$i' type='hidden'/>";
+				}
 			}
-			
-			$query_pi = mysql_query("select * from order_pijat where id_order='$_GET[id]'");
-			while($pi = mysql_fetch_array($query_pi)){
-				$tarif_pi = $pi[tarif];
-				echo "
-				<tr>
-					<td>$no</td>
-					<td colspan=5>Pijat</td>
-					<td>Rp. ".format_rupiah($tarif_pi)."</td>
-				</tr>";
-				$total_ekstra = ($total_ekstra + $tarif_pi);
-				$no++;
+			else{
+				echo "<script>alert('Maaf, proses downgrade class tidak diperbolehkan'); </script>";
 			}
-			
-			echo "
-				<tr>
-					<td colspan='6' align='right'><b>SUB TOTAL : </b></td>
-					<td><b>Rp. ".format_rupiah($total_ekstra)."</b></td>
-				</tr>
-				<tr>
-					<td colspan='6' align='right'><b>GRAND TOTAL : </b></td>
-					<td><b>Rp. ".format_rupiah($total_tarif+$total_ekstra)."</b></td>
-				</tr>
-				</tbody>
-			</table>";
-		break;
+		}
+
+		echo "</form>
+		<br>";
+
 	}
 }
 ?>
