75527df6fd3680a315403dbea86d7ec1d18365e5
Gamal Barus <gamalbarus@Gamals-MacBook-Pro.local>
2016-10-16 20:30:12 +0700

    add simulasi page and funtion

--- content.php
+++ content.php
@@ -5,38 +5,37 @@ switch ($_GET['page']){
 			include "page/koran.php";
 		}
 		break;
-	{
+	case "simulasi":
 		include "page/simulasi.php";
 		break;
-	}
 	case "pemesanan":
 		include "page/pemesanan.php";
 		break;
-		
+
 	case "faq":
 		include "page/faq.php";
 		break;
-		
+
 	case "saran-kritik":
 		include "page/saran-kritik.php";
 		break;
-		
+
 	case "profil":
 		include "page/profil.php";
 		break;
-		
+
 	case "agen":
 		include "page/agen.php";
 		break;
-		
+
 	case "promo":
 		include "page/promo.php";
 		break;
-		
+
 	case "makanan":
 		include "page/makanan.php";
 		break;
-		
+
 	case "jadwal":
 		include "page/jadwal.php";
 		break;
@@ -55,41 +54,41 @@ switch ($_GET['page']){
 
 	case "stop":
 		include "page/stop.php";
-		break;	
+		break;
 
 	case "konfirmasi":
 		include "page/konfirmasi.php";
-		break;	
+		break;
 
 	case "ubah-password":
 		if (isset($_SESSION[basyenkuser]) and isset($_SESSION[basyenkpassword]) and isset($_SESSION[basyenklevel])){
 			include "page/ubah-password.php";
 		}
 		break;
-		
+
 	case "akun":
 		if (isset($_SESSION[basyenkuser]) and isset($_SESSION[basyenkpassword]) and isset($_SESSION[basyenklevel])){
 			include "page/akun.php";
 		}
 		break;
-		
+
 	case "home":
 		include "page/home.php";
 		break;
-		
+
 	case "registrasi":
 		include "page/registrasi.php";
 		break;
-		
+
 	case "vote":
 		include "prose/vote.php";
-		break;	
-	
+		break;
+
 	case "logout":
 		session_destroy();
 		echo "<script>window.location = 'media.php?page=home'</script>";
 		break;
-	
+
 	default:
 		include "page/404.php";
 		break;
--- page/simulasi.php
+++ page/simulasi.php
@@ -1,20 +1,46 @@
 <h1>Simulasi Pemesanan</h1>
 <hr color='#000000' size='1'>
 
-<video width="670" controls = "controls">
-  <source src="video/kapal.mp4" type="video/mp4" >
-Your browser does not support the video tag.
-</video>
-<h1><center>Tahap-tahap Pemesanan Tiket Kapal</h1></center><br>
-<b>1. Registrasi <br>
-2. Login <br>
-3. Pilih Tiket <br>
-4. Data Pelanggan <br>
-5. Data Penumpang <br>
-6. Pilih Kursi <br>
-7. Pilih Layanan Tambahan <br>
-8. Pembayaran <br><br>
+	<div class="register form">
 
-*Pembayaran melalui proses transfer, maksimal 5 jam dari proses pemesanan<br>
-*Kemudian tiket akan dikirim melaui sms dan email
-*TERIMA KASIH* </b>
+		<form class="standard" action="" method="POST">
+		<div class="clearfix">
+
+
+		<!-- Status Pembayaran -->
+		<div class="clearfix" style="margin-top: 10px;margin-bottom:10px">
+			<div style="width:30%; float: left; padding-top: 8px">Jumlah Nominal Uang</div>
+      <input style="allign:left " placeholder="Rp." id="nominal" oninput="hitungHarga()">
+		</div>
+
+				<div class="clear"></div>
+
+		<!-- Total yang harus dibayar -->
+		<div class="clearfix" style="padding:15px; background-color:#125e9d">
+      <div style="width:40%; float: left;"><b style="color:#fefffc">Anda dapat memperoleh koran</b></div>
+			<div style="width:60%"><b id="jumlah_eksemplar" style="color:#fefffc">0</b><b style="color:#fefffc"> eksemplar</b></div>
+      <div style="width:40%; float: left;"><b style="color:#fefffc">Atau, berlangganan untuk</b></div>
+			<div style="width:60%"><b id="jumlah_langganan" style="color:#fefffc">0</b><b style="color:#fefffc"> bulan</b></div>
+		</div>
+
+    <script>
+    Number.prototype.format = function(n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+};
+
+    function hitungHarga(){
+      var nominal = document.getElementById("nominal").value;
+      if (isNaN(nominal)){
+        alert("Nominal tidak valid. Silakan masukkan bilangan");
+      }
+      else{
+        var eksemplar = Math.floor(nominal / 3500);
+        var langganan = Math.floor(nominal / 79000);
+        document.getElementById("jumlah_eksemplar").innerHTML = eksemplar;
+        document.getElementById("jumlah_langganan").innerHTML = langganan;
+        // alert(langganan);
+      }
+
+    }
+    </script>
