59bbb50bf5000c49efd22b46010005152f47f715
Gamal Barus <gamalbarus@Gamals-MacBook-Pro.local>
2016-10-18 08:50:26 +0700

    modify polling2 add function to limit displayed result

--- admin/modul/mod_polling2/polling2.php
+++ admin/modul/mod_polling2/polling2.php
@@ -7,13 +7,13 @@ switch($_GET[act]){
           <input type=button class='tombol' value='Tambah Data' onclick=\"window.location.href='?module=polling2&act=tambah';\">
           <table>
           <tr><th>No</th><th>Id Polling</th><th>Jawaban</th><th>IP 1</th><th>IP 2</th><th>Tahun</th>";
-    
+
 $p      = new Paging;
 $batas  = 10;
 $posisi = $p->cariPosisi($batas);
 
 
-    $tampil	= mysql_query("SELECT * FROM hasil_polling");
+    $tampil	= mysql_query("SELECT * FROM hasil_polling LIMIT $posisi, $batas");
 	  $no		= 1;
     while ($r=mysql_fetch_array($tampil)){
 		echo "<tr>
@@ -28,7 +28,7 @@ $posisi = $p->cariPosisi($batas);
 		$no++;
     }
     echo "</table>";
-    $jmldata=mysql_num_rows(mysql_query("SELECT * FROM hubungi"));
+    $jmldata=mysql_num_rows(mysql_query("SELECT * FROM hasil_polling"));
     $jmlhalaman  = $p->jumlahHalaman($jmldata, $batas);
     $linkHalaman = $p->navHalaman($_GET[halaman], $jmlhalaman);
 
@@ -41,8 +41,8 @@ echo "<div id=paging>Hal: $linkHalaman</div><br>";
           <table>
           <tr><td>Email</td> 	<td> : <input type=text name='username' size='50'></td></tr>
           <tr><td>Password</td>       	<td> : <textarea name='pass' style='width: 375px; height: 50px;'></textarea></td></tr>
-          <tr><td>Group</td>      <td> : <input type=radio name='grup' value='0'>0 
-                                         <input type=radio name='grup' value='1'>1  
+          <tr><td>Group</td>      <td> : <input type=radio name='grup' value='0'>0
+                                         <input type=radio name='grup' value='1'>1
                                          <input type=radio name='grup' value='2'>2</td></tr>
            <tr><td>Status</td>   <td> : <input type=text name='status' size='50'></td></tr>
 
@@ -50,6 +50,6 @@ echo "<div id=paging>Hal: $linkHalaman</div><br>";
                             <input type=button class='tombol' value=Batal onclick=self.history.back()></td></tr>
           </table></form>";
      break;
- 
+
 }
 ?>
