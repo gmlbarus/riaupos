cfbebc3a3be4f703e0dc989ac17993ec77991478
gmlbarus <gmlbarus@gmail.com>
2016-10-30 21:38:40 +0700

    Modify mod_polling2/aksi Fix error when access array name and add phone number validation

--- admin/modul/mod_polling2/aksi.php
+++ admin/modul/mod_polling2/aksi.php
@@ -1,19 +1,29 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 include "../../../config/koneksi.php";
 
-$module=$_GET[module];
-$act=$_GET[act];
+$module=$_GET['module'];
+$act=$_GET['act'];
 
-if ($_POST[pollingSms] == 'Vote!')
-    if ($_POST[noHp] != ''){
-      $noHp = $_POST[noHp];
+if ($_POST['pollingSms'] == 'Vote!')
+    if ($_POST['noHp'] != ''){
+      $noHp = $_POST['noHp'];
+
+      if (! is_numeric($noHp))
+          echo "<script>alert('No HP tidak valid');history.back();</script>";
+
       $tahun = date('Y');
+
       $query = "SELECT * FROM `hasil_polling_sms` WHERE `noHp` = '{$noHp}' and `tahun` = '{$tahun}'";
       $hasVoted = mysql_num_rows(mysql_query($query));
+
       if ($hasVoted){
         echo "<script>alert('Voting sudah pernah dilakukan sebelumnya untuk nomor ini');history.back()</script>";
       }
+
       else {
         $q = mysql_query("SELECT * FROM polling");
       	$i=1;
@@ -25,7 +35,7 @@ if ($_POST[pollingSms] == 'Vote!')
         }
 
     	}
-      echo "<script>alert('Data polling tersimpan');history.back()</script>";
+      echo "<script>alert('Data polling tersimpan');history.back();location.reload(true)</script>";
     }
     else
       echo "<script>alert('masukkan No Handphone');history.back()</script>";
